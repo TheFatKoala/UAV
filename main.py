7312c3cfab7ffef14bb545ac0f0182c1e49e707f
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-02-26 10:43:41 -0500

    stuff

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from extract import extract
 import tensorflow as tf
 import numpy as np
 
-result = extract(fr"C:\Users\hi2kh\OneDrive\Documents\GitHub\Machine-Learning\image.png", [245, 220, 154])
+result = extract(fr"C:\Users\hi2kh\OneDrive\Documents\GitHub\Machine-Learning\image.png", [206, 197, 205])
 result = np.reshape(result, (1, 28, 28, 1))
-model = tf.keras.models.load_model(r'letter_classifier/letter_model')
+model = tf.keras.models.load_model(r'classify/classify_model')
 prediction = model.predict(result)
-print(type(prediction))
+#print(type(prediction))
 print(prediction)
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-index = np.argmax(prediction)
-print(alphabet[index])
+#alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#index = np.argmax(prediction)
+#print(alphabet[index])
+
