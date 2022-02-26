9490caab2e13898fe28ed3dfff2e4cee4da2425d
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-02-26 10:35:24 -0500

    stuff

--- extract.py
+++ extract.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-def extract(color):  # color = [R, G, B]
+def extract(image_path, color) -> np.array:  # color = [R, G, B]
     # returns processed image as black and white (0, 255)
-    image = cv2.imread(r'C:\Users\hi2kh\OneDrive\Desktop\Python\Letter Recognition\image.png')
+    image = cv2.imread(fr'{image_path}')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     lower = np.array(color, dtype="uint8") - 10
@@ -16,10 +16,12 @@ def extract(color):  # color = [R, G, B]
     result = cv2.bitwise_and(gray, gray, mask=mask)
     (thresh, result) = cv2.threshold(result, 90, 255, cv2.THRESH_BINARY)
     #TODO: Center image
+    result = cv2.resize(result, (28, 28))
+    return result  # np.array
+    #cv2.imshow("Image", result)
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
 
-    cv2.imshow("Image", result)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
 
-#extract([245,220,154])
-extract([206,197,205])
+#extract([245, 220, 154])  # letter
+#extract([206, 197, 205])  # shape
